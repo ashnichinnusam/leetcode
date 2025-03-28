b108a557f167da6a18ff40e5fb0ebf8b98a7a694
ashnichinnusam <ashnichinnusam2003@gmail.com>
2025-03-28 15:24:51 +0530

    added sliding window

---
+++ LongestSubArraydltOne.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        start = 0
+        zero = -1
+        max_len = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 0 :
+                start = zero+1
+                zero = i
+            max_len = max(max_len,i-start)
+        return max_len 
+        
